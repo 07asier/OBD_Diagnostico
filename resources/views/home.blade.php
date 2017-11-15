4643b3ea790f61e90f3fa7ada28ebfc482b4d7e8
“07asier” <asier2223@gmail.com>
2017-11-15 20:54:50 +0100

    full

---
+++ resources/views/home.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OBD Diagnostico</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="css/estiloshome.css" rel="stylesheet">
+
+
+</head>
+
+<body >
+
+<!-- Navigation -->
+<nav class="navbar-inverse" id="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#" id="titulo">Racegnostic</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="" id="menus">Inicio</a></li>
+                <li><a href="" id="menus">Quienes somos</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="register2" id="menus"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
+                <li><a href="login2" id="menus"><span class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<header class="masthead text-center text-white d-flex">
+    <div class="container my-auto">
+        <div class="row">
+            <div >
+                <h1 class="text-uppercase">
+                    <p>OBTEN LA INFO DE TU COCHE.</p>
+                    <p>EN UN ABRIR Y CERRAR DE OJOS.</p>
+                </h1>
+
+            </div>
+            <div>
+
+                <p class="blanco">¿Quieres conocernos?</p>
+                <a class="btn btn-primary btn-lg active"  aria-pressed="true" href="#services">Descubrenos</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">¿Por qué nosotros?</h2>
+                <hr class="my-4">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/fast.png"> </img>
+                    <h3>Rápido</h3>
+                    <p class="text-muted mb-0">Diagnóstico a tiempo real</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/security.png"> </img>
+                    <h3 >Fiable</h3>
+                    <p class="text-muted mb-0">Servicio 100% fiable</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/coche.png"> </img>
+                    <h3>Multifuncional</h3>
+                    <p class="text-muted mb-0">Ofrecemos la posibilidad de añadir varios coches</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="service-box mt-5 mx-auto">
+                    <img src="images/icons/easy.png"> </img>
+                    <h3>Accesibilidad</h3>
+                    <p class="text-muted mb-0">Fácil acceso a los datos</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="my-4">
+</section>
+
+<section>
+    <div class="container my-auto">
+        <div class="row">
+            <div>
+                <h3 class="text-center">Quiénes somos</h3>
+                <p class="text-center">Somos una pequeña empresa del ámbito automovilistico, que analizamos a tiempo real(a través de 4G) los datos de un coche.Mediante un <a href="https://es.wikipedia.org/wiki/OBD">OBD-II </a> almacenamos los datos en la <a href="https://es.wikipedia.org/wiki/Computaci%C3%B3n_en_la_nube" >nube</a>, y luego los plasmamos en ésta aplicación web. Es un servicio rápido, fiable y sobre todo sencillo de usar. Ofrecemos la opción de añadir más de un vehiculo a la aplicación. </p>
+                <h3 class="text-center">Cómo funciona</h3>
+                <p class="text-center"><li><strong>Paso 1:</strong></li> </br> Enchufar el OBD-II en la ranura del coche </p>
+                <p class="text-center"><li><strong>Paso 2:</strong></li> </br> Introducir el número de serie del OBD-II en el coche, y configurar los datos del coche. </p>
+                <p class="text-center"><li><strong>Paso 3:</strong></li> </br> Listo! </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</body>
+
+
+</html>
